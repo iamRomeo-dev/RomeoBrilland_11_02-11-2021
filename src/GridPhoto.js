6af3c7699d81a9a669bdaf283589155bcff1e594
iamRomeo-dev <romeo.brilland@live.fr>
2021-11-02 15:42:34 +0100

    💄 Display the title of the image

--- src/GridPhoto.js
+++ src/GridPhoto.js
@@ -4,16 +4,13 @@ export const GridPhoto = ({ data }) => {
       <li className="relative h-60 col-span-1 flex flex-col  md:h-72 lg:h-80 xl:h-96">
         <img
           src={data.cover}
-          alt="new"
+          alt={data.title}
           className="object-cover h-full rounded-xl"
         />
-      </li>
-
-      {/* <li class="relative h-60 col-span-1 flex flex-col bg-red-200 rounded-xl md:h-72 lg:h-80 xl:h-96">
-        <span class="absolute bottom-5 left-5 text-sm font-normal tracking-normal text-white tk-brandon-grotesque">
+        <span className="absolute bottom-5 left-5 text-sm font-normal tracking-normal text-white tk-brandon-grotesque">
           {data.title}
         </span>
-      </li> */}
+      </li>
     </>
   );
 };
