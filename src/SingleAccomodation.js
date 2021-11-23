4cd3c0ff93674fac933e77a3c3110aff707e08d4
iamRomeo-dev <romeo.brilland@live.fr>
2021-11-23 20:19:35 +0100

    Fix get single data under function

--- src/SingleAccomodation.js
+++ src/SingleAccomodation.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import Header from "./Header";
 import { DisplayStars } from "./DisplayStars";
 import { data } from "./data";
-import SingleBanner from "./SingleBanner";
-import Banner from "./Banner";
 import { LeftArrowIcon, RightArrowIcon } from "./Icons";
 
 export default class SingleAccomodation extends Component {
-  render() {
-    const SingleAccomodationDatas = data.filter((accomodation) => {
+  SingleAccomodationDatas = () => {
+    return data.filter((accomodation) => {
       return accomodation.id === this.props.match.params.id;
     })[0];
+  };
 
+  render() {
+    // console.log("toto", this.SingleAccomodationDatas());
     return (
       <>
         <>
@@ -19,7 +20,7 @@ export default class SingleAccomodation extends Component {
           {/* <SingleBanner /> */}
           <div className="relative">
             <img
-              src={SingleAccomodationDatas.pictures[0]}
+              src={this.SingleAccomodationDatas().pictures[0]}
               alt="SingleBanner"
               className="object-cover h-72 w-full rounded-xl"
             />
@@ -27,7 +28,7 @@ export default class SingleAccomodation extends Component {
               // onClick={handlePreviousPicture}
               className="absolute left-2 top-1/2 -translate-y-1/2"
             >
-              {SingleAccomodationDatas.pictures.length != 1 && (
+              {this.SingleAccomodationDatas().pictures.length !== 1 && (
                 <LeftArrowIcon />
               )}
             </div>
@@ -35,7 +36,7 @@ export default class SingleAccomodation extends Component {
               // onClick={handleNextPicture}
               className="absolute right-2 top-1/2 -translate-y-1/2"
             >
-              {SingleAccomodationDatas.pictures.length != 1 && (
+              {this.SingleAccomodationDatas().pictures.length !== 1 && (
                 <RightArrowIcon />
               )}
             </div>
@@ -46,14 +47,14 @@ export default class SingleAccomodation extends Component {
             <div>
               <div>
                 <h1 className="text-base md:text-3xl text-red-400 font-normal">
-                  {SingleAccomodationDatas.title}
+                  {this.SingleAccomodationDatas().title}
                 </h1>
                 <h3 className="text-xs md:text-lg text-red-400 font-normal">
-                  {SingleAccomodationDatas.location}
+                  {this.SingleAccomodationDatas().location}
                 </h3>
               </div>
               <ul className="flex flex-wrap gap-2 mt-2">
-                {SingleAccomodationDatas.tags.map((tag, index) => (
+                {this.SingleAccomodationDatas().tags.map((tag, index) => (
                   <li className="text-white text-xs md:text-lg bg-red-500 rounded-3xl px-4 md:px-8">
                     {tag}
                   </li>
@@ -63,11 +64,11 @@ export default class SingleAccomodation extends Component {
             <div class="flex flex-row-reverse items-center justify-between gap-4">
               <div className="flex justify-center items-center">
                 <h3 className="text-xs md:text-lg text-red-400 font-normal">
-                  {SingleAccomodationDatas.host.name}
+                  {this.SingleAccomodationDatas().host.name}
                 </h3>
                 <div class="rounded-full bg-gray-200 h-12 w-12 ml-2 flex items-center justify-center"></div>
               </div>
-              <DisplayStars rating={SingleAccomodationDatas.rating} />
+              <DisplayStars rating={this.SingleAccomodationDatas().rating} />
             </div>
           </div>
         </>
