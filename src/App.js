10d39b749497ccaa4e1fb26c81730b63a0375ce5
iamRomeo-dev <romeo.brilland@live.fr>
2021-11-29 13:01:32 +0100

    :sparkles: Add Footer

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import SingleAccomodation from "./SingleAccomodation";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="mx-6 sm:mx-24">
-        <Switch>
-          <Route
-            exact
-            path="/accomodation/:id"
-            component={SingleAccomodation}
-          />
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          {/* <Route path="*">
+      <>
+        <div className="mx-6 sm:mx-24">
+          <Switch>
+            <Route
+              exact
+              path="/accomodation/:id"
+              component={SingleAccomodation}
+            />
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            {/* <Route path="*">
             <Redirect to="/page-not-found" />
           </Route> */}
-        </Switch>
-      </div>
+          </Switch>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { LogoWhite } from "./Icons";
+class Footer extends Component {
+  render() {
+    return (
+      <div className="bg-black flex flex-col justify-center items-center py-8 mt-6">
+        <Link to="/">
+          <LogoWhite />
+        </Link>
+        <div className="text-white">© 2020 Kasa. All rigths reserved</div>
+      </div>
+    );
+  }
+}
+export default Footer;
