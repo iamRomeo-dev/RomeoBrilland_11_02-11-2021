d87f68edcadf846f76293002b066fe758c448c76
iamRomeo-dev <romeo.brilland@live.fr>
2021-11-29 14:33:18 +0100

    Put DisplayStars component on class component

--- src/DisplayStars.js
+++ src/DisplayStars.js
@@ -1,64 +1,68 @@
+import React, { Component } from "react";
 import { StarFilledIcon } from "./Icons";
 import { StarOutlinedIcon } from "./Icons";
 
-export const DisplayStars = ({ rating }) => {
-  return (
-    <>
-      {rating === "0" && (
-        <div className="flex">
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-        </div>
-      )}
+export class DisplayStars extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <>
+        {rating === "0" && (
+          <div className="flex">
+            <StarOutlinedIcon className="text-red-500 bg-blue-500" />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+          </div>
+        )}
 
-      {rating === "1" && (
-        <div className="flex">
-          <StarFilledIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-        </div>
-      )}
-      {rating === "2" && (
-        <div className="flex">
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-        </div>
-      )}
-      {rating === "3" && (
-        <div className="flex">
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-        </div>
-      )}
-      {rating === "4" && (
-        <div className="flex">
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarOutlinedIcon />
-        </div>
-      )}
-      {rating === "5" && (
-        <div className="flex">
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarFilledIcon />
-          <StarFilledIcon />
-        </div>
-      )}
-    </>
-  );
-};
+        {rating === "1" && (
+          <div className="flex">
+            <StarFilledIcon className="#f87171" />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+          </div>
+        )}
+        {rating === "2" && (
+          <div className="flex">
+            <StarFilledIcon />
+            <StarFilledIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+          </div>
+        )}
+        {rating === "3" && (
+          <div className="flex">
+            <StarFilledIcon className="text-red-500 bg-blue-500" />
+            <StarFilledIcon />
+            <StarFilledIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+          </div>
+        )}
+        {rating === "4" && (
+          <div className="flex">
+            <StarFilledIcon className="text-red-500 bg-blue-500" />
+            <StarFilledIcon />
+            <StarFilledIcon />
+            <StarFilledIcon />
+            <StarOutlinedIcon />
+          </div>
+        )}
+        {rating === "5" && (
+          <div className="flex">
+            <StarFilledIcon className="text-red-500 bg-blue-500" />
+            <StarFilledIcon />
+            <StarFilledIcon />
+            <StarFilledIcon />
+            <StarFilledIcon />
+          </div>
+        )}
+      </>
+    );
+  }
+}
