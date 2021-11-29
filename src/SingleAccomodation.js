6f94afa77ba81f36206a214e652a21515238950b
iamRomeo-dev <romeo.brilland@live.fr>
2021-11-29 11:45:54 +0100

    :sparkles: Fix single banner with previous and next picture

--- src/SingleAccomodation.js
+++ src/SingleAccomodation.js
@@ -3,75 +3,120 @@ import Header from "./Header";
 import { DisplayStars } from "./DisplayStars";
 import { data } from "./data";
 import { LeftArrowIcon, RightArrowIcon } from "./Icons";
+import Dropdown from "./Dropdown";
 
 export default class SingleAccomodation extends Component {
+  // this.state.index sera l'index de départ, donc 0. Donc affichera la premiere photo
+  constructor() {
+    super();
+    this.state = {
+      index: 0,
+    };
+  }
+
+  // Fonction qui affiche les données filtré par id qui est récupéré dans l'url
   SingleAccomodationDatas = () => {
     return data.filter((accomodation) => {
       return accomodation.id === this.props.match.params.id;
     })[0];
   };
 
+  // Index +1 par rapport à la longueur du tableau
+  HandleNextPicture = () => {
+    this.setState({
+      index:
+        (this.state.index + 1) % this.SingleAccomodationDatas().pictures.length,
+    });
+  };
+
+  // Index -1 par rapport à la longueur du tableau sauf si index = 0 alors on set le state à la longueur du tableau -1, pour repartir à la dernière photo
+  HandlePreviousPicture = () => {
+    if (this.state.index === 0) {
+      return this.setState({
+        index: this.SingleAccomodationDatas().pictures.length - 1,
+      });
+    } else {
+      this.setState({
+        index:
+          (this.state.index - 1) %
+          this.SingleAccomodationDatas().pictures.length,
+      });
+    }
+  };
+
   render() {
-    // console.log("toto", this.SingleAccomodationDatas());
+    const item = this.SingleAccomodationDatas().pictures[this.state.index];
     return (
       <>
-        <>
-          <Header />
-          {/* <SingleBanner /> */}
-          <div className="relative">
-            <img
-              src={this.SingleAccomodationDatas().pictures[0]}
-              alt="SingleBanner"
-              className="object-cover h-72 w-full rounded-xl"
-            />
-            <div
-              // onClick={handlePreviousPicture}
-              className="absolute left-2 top-1/2 -translate-y-1/2"
-            >
-              {this.SingleAccomodationDatas().pictures.length !== 1 && (
-                <LeftArrowIcon />
-              )}
-            </div>
-            <div
-              // onClick={handleNextPicture}
-              className="absolute right-2 top-1/2 -translate-y-1/2"
-            >
-              {this.SingleAccomodationDatas().pictures.length !== 1 && (
-                <RightArrowIcon />
-              )}
-            </div>
+        <Header />
+        {/* <SingleBanner /> */}
+        <div className="relative">
+          <img
+            src={item}
+            alt="SingleBanner"
+            className="object-cover h-72 w-full rounded-xl"
+          />
+          <div
+            onClick={this.HandlePreviousPicture}
+            className="absolute left-2 top-1/2 -translate-y-1/2"
+          >
+            {this.SingleAccomodationDatas().pictures.length !== 1 && (
+              <LeftArrowIcon />
+            )}
           </div>
-          {/* <SingleBanner /> */}
+          <div
+            onClick={this.HandleNextPicture}
+            className="absolute right-2 top-1/2 -translate-y-1/2"
+          >
+            {this.SingleAccomodationDatas().pictures.length !== 1 && (
+              <RightArrowIcon />
+            )}
+          </div>
+        </div>
+        {/* <SingleBanner /> */}
 
-          <div className="sm:flex justify-between flex-wrap mt-6">
+        <div className="sm:flex justify-between flex-wrap mt-6">
+          <div>
             <div>
-              <div>
-                <h1 className="text-base md:text-3xl text-red-400 font-normal">
-                  {this.SingleAccomodationDatas().title}
-                </h1>
-                <h3 className="text-xs md:text-lg text-red-400 font-normal">
-                  {this.SingleAccomodationDatas().location}
-                </h3>
-              </div>
-              <ul className="flex flex-wrap gap-2 mt-2">
-                {this.SingleAccomodationDatas().tags.map((tag, index) => (
-                  <li className="text-white text-xs md:text-lg bg-red-500 rounded-3xl px-4 md:px-8">
-                    {tag}
-                  </li>
-                ))}
-              </ul>
+              <h1 className="text-base md:text-3xl text-red-400 font-normal">
+                {this.SingleAccomodationDatas().title}
+              </h1>
+              <h3 className="text-xs md:text-lg text-red-400 font-normal">
+                {this.SingleAccomodationDatas().location}
+              </h3>
             </div>
-            <div class="flex flex-row-reverse items-center justify-between gap-4">
-              <div className="flex justify-center items-center">
-                <h3 className="text-xs md:text-lg text-red-400 font-normal">
-                  {this.SingleAccomodationDatas().host.name}
-                </h3>
-                <div class="rounded-full bg-gray-200 h-12 w-12 ml-2 flex items-center justify-center"></div>
-              </div>
-              <DisplayStars rating={this.SingleAccomodationDatas().rating} />
-            </div>
+            <ul className="flex flex-wrap gap-2 mt-2">
+              {this.SingleAccomodationDatas().tags.map((tag, index) => (
+                <li
+                  key={index}
+                  className="text-white text-xs md:text-lg bg-red-500 rounded-3xl px-4 md:px-8"
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
           </div>
-        </>
+          <div className="flex flex-row-reverse items-center justify-between gap-4">
+            <div className="flex justify-center items-center">
+              <h3 className="text-xs md:text-lg text-red-400 font-normal">
+                {this.SingleAccomodationDatas().host.name}
+              </h3>
+              <div class="rounded-full bg-gray-200 h-12 w-12 ml-2 flex items-center justify-center"></div>
+            </div>
+            <DisplayStars rating={this.SingleAccomodationDatas().rating} />
+          </div>
+        </div>
+
+        <div className="flex gap-4 mt-6">
+          <Dropdown
+            title="Description"
+            content={this.SingleAccomodationDatas().description}
+          />
+          <Dropdown
+            title="Equipements"
+            content={this.SingleAccomodationDatas().equipments}
+          />
+        </div>
       </>
     );
   }
