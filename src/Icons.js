4fa8a5c014e07c1b4103aa5b59de62ec5f16e1c8
iamRomeo-dev <romeo.brilland@live.fr>
2021-11-29 14:36:41 +0100

    :lipstick: Fix color of stars

--- src/Icons.js
+++ src/Icons.js
@@ -2,19 +2,14 @@ export const StarOutlinedIcon = () => {
   return (
     <svg
       stroke="currentColor"
-      fill="currentColor"
+      fill="#e3e3e3"
       strokeWidth="0"
       viewBox="0 0 512 512"
       height="1em"
       width="1em"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        fill="none"
-        strokeLinejoin="round"
-        strokeWidth="32"
-        d="M480 208H308L256 48l-52 160H32l140 96-54 160 138-100 138 100-54-160z"
-      ></path>
+      <path d="M496 203.3H312.36L256 32l-56.36 171.3H16l150.21 105.4-58.5 171.3L256 373.84 404.29 480l-58.61-171.3z"></path>
     </svg>
   );
 };
@@ -23,7 +18,7 @@ export const StarFilledIcon = () => {
   return (
     <svg
       stroke="currentColor"
-      fill="currentColor"
+      fill="#f87171"
       strokeWidth="0"
       viewBox="0 0 512 512"
       height="1em"
